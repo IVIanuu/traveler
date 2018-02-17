82b9038708e4ff89cdb5193b36e57eda7a97c989
IVIanuu <IVIanuu@gmail.com>
2018-02-17 14:30:25 +0100

    Gradle change

--- traveler-keys/src/main/java/com/ivianuu/traveler/keys/KeyNavigator.kt
+++ traveler-keys/src/main/java/com/ivianuu/traveler/keys/KeyNavigator.kt
@@ -102,7 +102,7 @@ open class KeyNavigator(
         )
 
         fragmentTransaction
-            .replace(containerId, fragment)
+            .replace(containerId, fragment, tag)
             .addToBackStack(tag)
             .commit()
 
@@ -154,7 +154,7 @@ open class KeyNavigator(
             )
 
             fragmentTransaction
-                .replace(containerId, fragment)
+                .replace(containerId, fragment, tag)
                 .addToBackStack(tag)
                 .commit()
             localStackCopy.add(tag)
@@ -169,7 +169,7 @@ open class KeyNavigator(
             )
 
             fragmentTransaction
-                .replace(containerId, fragment)
+                .replace(containerId, fragment, tag)
                 .commit()
         }
     }
