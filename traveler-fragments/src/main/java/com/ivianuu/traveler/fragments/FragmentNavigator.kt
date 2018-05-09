ce2d8b597ae78a366389053ac0d9526ea7d125f1
IVIanuu <IVIanuu@gmail.com>
2018-05-09 12:05:11 +0200

    Extraced fragment navigator logic out to a helper

--- traveler-fragments/src/main/java/com/ivianuu/traveler/fragments/FragmentNavigator.kt
+++ traveler-fragments/src/main/java/com/ivianuu/traveler/fragments/FragmentNavigator.kt
@@ -16,188 +16,53 @@
 
 package com.ivianuu.traveler.fragments
 
-import android.support.v4.app.DialogFragment
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
 import com.ivianuu.traveler.BaseNavigator
 import com.ivianuu.traveler.commands.*
-import java.util.*
 
 /**
  * Navigator for fragments only
  */
 abstract class FragmentNavigator(
-    private val fragmentManager: FragmentManager,
-    private val containerId: Int
-): BaseNavigator() {
+    fragmentManager: FragmentManager,
+    containerId: Int
+): BaseNavigator(), FragmentNavigatorHelper.Callback {
 
-    private val localStackCopy = LinkedList<String>()
-
-    override fun applyCommands(commands: Array<Command>) {
-        try {
-            fragmentManager.executePendingTransactions()
-        } catch (e: Exception) {
-            // ignore
-        }
-
-        //copy stack before apply commands
-        copyStackToLocal()
-
-        super.applyCommands(commands)
-    }
+    private val fragmentNavigatorHelper = FragmentNavigatorHelper(this, fragmentManager, containerId)
 
     override fun forward(command: Forward) {
-        val fragment = createFragment(command.key, command.data)
-
-        if (fragment == null) {
+        if (!fragmentNavigatorHelper.forward(command)) {
             unknownScreen(command)
-            return
         }
-
-        val tag = getFragmentTag(command.key)
-
-        val transaction = fragmentManager.beginTransaction()
-
-        setupFragmentTransaction(
-            command,
-            fragmentManager.findFragmentById(containerId),
-            fragment,
-            transaction
-        )
-
-        if (fragment !is DialogFragment) {
-            transaction
-                .replace(containerId, fragment, tag)
-                .addToBackStack(tag)
-                .commit()
-        } else {
-            transaction.addToBackStack(tag)
-            fragment.show(transaction, tag)
-        }
-
-        localStackCopy.add(tag)
     }
 
     override fun replace(command: Replace) {
-        val fragment = createFragment(command.key, command.data)
-
-        if (fragment == null) {
+        if (!fragmentNavigatorHelper.replace(command)) {
             unknownScreen(command)
-            return
-        }
-
-        val tag = getFragmentTag(command.key)
-
-        if (localStackCopy.size > 0) {
-            fragmentManager.popBackStack()
-            localStackCopy.pop()
-
-            val transaction = fragmentManager.beginTransaction()
-
-            setupFragmentTransaction(
-                command,
-                fragmentManager.findFragmentById(containerId),
-                fragment,
-                transaction
-            )
-
-            if (fragment !is DialogFragment) {
-                transaction
-                    .replace(containerId, fragment, tag)
-                    .addToBackStack(tag)
-                    .commit()
-            } else {
-                transaction.addToBackStack(tag)
-                fragment.show(transaction, tag)
-            }
-
-            localStackCopy.add(tag)
-        } else {
-            val transaction = fragmentManager.beginTransaction()
-
-            setupFragmentTransaction(
-                command,
-                fragmentManager.findFragmentById(containerId),
-                fragment,
-                transaction
-            )
-
-            if (fragment !is DialogFragment) {
-                transaction
-                    .replace(containerId, fragment, tag)
-                    .commit()
-            } else {
-                transaction.addToBackStack(tag)
-                fragment.show(transaction, tag)
-            }
         }
     }
 
     override fun back(command: Back) {
-        if (localStackCopy.size > 0) {
-            fragmentManager.popBackStack()
-            localStackCopy.pop()
-        } else {
+        if (!fragmentNavigatorHelper.back(command)) {
             exit()
         }
     }
 
     override fun backTo(command: BackTo) {
-        val key = command.key
-
-        if (key == null) {
-            backToRoot()
-        } else {
-            val tag = getFragmentTag(key)
-
-            val index = localStackCopy.indexOf(tag)
-            val size = localStackCopy.size
-
-            if (index != -1) {
-                for (i in 1 until size - index) {
-                    localStackCopy.pop()
-                }
-                fragmentManager.popBackStack(tag, 0)
-            } else {
-                backToUnexisting(command)
-            }
+        if (!fragmentNavigatorHelper.backTo(command)) {
+            backToUnexisting(command.key!!)
         }
     }
 
-    open fun setupFragmentTransaction(
-        command: Command,
-        currentFragment: Fragment?,
-        nextFragment: Fragment,
-        transaction: FragmentTransaction
-    ) {
-    }
+    protected abstract fun exit()
 
     protected open fun backToUnexisting(key: Any) {
-        backToRoot()
+        fragmentNavigatorHelper.backToRoot()
     }
 
     protected open fun unknownScreen(command: Command) {
         throw IllegalArgumentException("unknown screen $command")
     }
 
-    protected open fun getFragmentTag(key: Any): String {
-        return key.toString()
-    }
 
-    protected abstract fun createFragment(key: Any, data: Any?): Fragment?
-
-    protected abstract fun exit()
-
-    private fun copyStackToLocal() {
-        localStackCopy.clear()
-
-        val stackSize = fragmentManager.backStackEntryCount
-        (0 until stackSize).mapTo(localStackCopy) { fragmentManager.getBackStackEntryAt(it).name }
-    }
-
-    private fun backToRoot() {
-        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-        localStackCopy.clear()
-    }
 }
