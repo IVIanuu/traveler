bad12c82e77bd1c16e9eb5c7e0290e0a0afea9e1
IVIanuu <IVIanuu@gmail.com>
2018-09-21 01:17:08 +0200

    Updated kotlin

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -13,7 +13,7 @@ object Versions {
     const val androidx = "1.0.0-rc02"
     const val androidxArch = "2.0.0-rc01"
 
-    const val kotlin = "1.3-M2"
+    const val kotlin = "1.3.0-rc-57"
     const val mavenGradlePlugin = "2.1"
 }
 
