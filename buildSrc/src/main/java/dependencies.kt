6dadc0c9576d81c88f21915eb6ddca04d4ecaaba
IVIanuu <IVIanuu@gmail.com>
2018-09-27 16:16:32 +0200

    Local maven

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -6,14 +6,15 @@ object Versions {
     const val minSdk = 16
     const val targetSdk = 28
     const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionName = "SNAPSHOT"
+    const val groupId = "com.ivianuu.traveler"
 
     const val androidGradlePlugin = "3.2.0"
 
     const val androidx = "1.0.0-rc02"
     const val androidxArch = "2.0.0-rc01"
 
-    const val director = "6fef661154"
+    const val director = "SNAPSHOT"
 
     const val kotlin = "1.3.0-rc-57"
     const val mavenGradlePlugin = "2.1"
@@ -28,7 +29,7 @@ object Deps {
     const val archLifecycleExtensions =
         "androidx.lifecycle:lifecycle-extensions:${Versions.androidxArch}"
 
-    const val director = "com.github.IVIanuu:director:${Versions.director}"
+    const val director = "com.ivianuu.director:director:${Versions.director}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
